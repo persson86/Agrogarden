b36fc4bdc71c2e71d7033efd80371afe2bab6bac
Luiz Felipe Schneider Persson <LFSPersson@MacBook-Air-de-Luiz.local>
2016-10-10 09:39:53 -0300

    V8

--- app/src/main/java/com/mob/schneiderpersson/agrohortav1/PlantAdapter.java
+++ app/src/main/java/com/mob/schneiderpersson/agrohortav1/PlantAdapter.java
@@ -1,6 +1,7 @@
 package com.mob.schneiderpersson.agrohortav1;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +14,14 @@ import java.util.ArrayList;
 public class PlantAdapter extends BaseAdapter {
 
     private final Context mContext;
-    ArrayList<Plant>  plants = new ArrayList<>();
+    //ArrayList<Plant>  plants = new ArrayList<>();
+    ArrayList<String> plants = new ArrayList<>();
+    ArrayList<String> listColor = new ArrayList<>();
 
-    public PlantAdapter(Context context, ArrayList<Plant> plants) {
+    public PlantAdapter(Context context, ArrayList<String> plants, ArrayList<String> color) {
         this.mContext = context;
         this.plants = plants;
+        this.listColor = color;
     }
 
     @Override
@@ -38,19 +42,44 @@ public class PlantAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        final Plant plant = plants.get(position);
+        //final Plant plant = plants.get(position);
+        String p = plants.get(position);
+
+        String c;
+
+        if (!listColor.contains("b")) {
+            if (listColor.size() > 0)
+                c = listColor.get(position);
+            else
+                c = "n";
+        }
+        else
+            c = "b";
 
         if (convertView == null) {
             final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
             convertView = layoutInflater.inflate(R.layout.custom_item, null);
         }
 
-        final ImageView imageView = (ImageView)convertView.findViewById(R.id.ivImage);
-        final TextView nameTextView = (TextView)convertView.findViewById(R.id.tvName);
+        int color = 0;
+
+        if (c.equals("c"))
+            color = 0xff00ff00;
+        else if (c.equals("a"))
+            color = 0xffff0000;
+        else if (c.equals("b"))
+            color = 0xffcccccc;
+        else
+            color = 0xff888888;
+
+        convertView.setBackgroundColor(color);
+
+        final ImageView imageView = (ImageView) convertView.findViewById(R.id.ivImage);
+        final TextView nameTextView = (TextView) convertView.findViewById(R.id.tvName);
 
         //imageView.setImageResource(plant.getImageResource());
         imageView.setImageResource(R.drawable.img_hortela);
-        nameTextView.setText(plant.getName());
+        nameTextView.setText(p);
         //nameTextView.setText(mContext.getString(plant.getName(position)));
 
         return convertView;
